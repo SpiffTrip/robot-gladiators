d68072b1278e5fa5193f39c212e995b180c07af0
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-01-06 16:02:41 -0700

    fixed fights and round based fights

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,11 +17,12 @@ var enemyAttack = 12;
 
 
 
-var fight = function (enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+var fight = function(enemyName) {
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
 
     // if player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -77,8 +78,11 @@ var fight = function (enemyName) {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
